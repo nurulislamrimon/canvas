2c2347cff5018b319237cb75bf20bed11f387dcf
nurulislamrimon <nurulislamrimon@gmail.com>
2024-09-03 21:21:03 +0600

    class 1 done

--- canvas-resize/index.js
+++ canvas-resize/index.js
@@ -1,3 +1,13 @@
+// selection
 const canvas = document.querySelector("canvas");
+
+// modification
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+
+ctx.fillRect(10, 10, 20, 20);
+ctx.fillRect(30, 50, 20, 20);
+ctx.fillRect(70, 30, 20, 20);
+ctx.fillRect(100, 10, 20, 20);
