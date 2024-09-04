5f2b68c2150109a81256c2c77ebd4124e872162e
nurulislamrimon <nurulislamrimon@gmail.com>
2024-09-04 12:51:07 +0600

    canvas animation done

--- canvas/index.js
+++ canvas/index.js
@@ -5,39 +5,57 @@ const canvas = document.querySelector("canvas");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+// context
 const ctx = canvas.getContext("2d");
 
-// rectangle
-ctx.fillStyle = "blue";
-ctx.fillRect(10, 10, 20, 20);
-ctx.fillStyle = "rgba(255,255,10)";
-ctx.fillRect(30, 50, 20, 20);
-ctx.fillStyle = "rgb(0,220,60)";
-ctx.fillRect(70, 30, 20, 20);
-ctx.fillStyle = "#f2105432";
-ctx.fillRect(100, 10, 20, 20);
+// circle instance
+class Circle {
+  constructor(x, y, dx, dy, radius) {
+    this.x = x;
+    this.y = y;
 
-// line
-ctx.beginPath();
-ctx.moveTo(100, 100);
-ctx.lineTo(150, 350);
-ctx.lineTo(250, 450);
-ctx.strokeStyle = "red";
-ctx.stroke();
+    this.dx = dx;
+    this.dy = dy;
 
-// circle
-ctx.arc(200, 200, 50, 0, Math.PI * 2, true);
-ctx.stroke();
+    this.draw = function () {
+      ctx.beginPath();
+      ctx.arc(x, y, radius, 0, Math.PI * 2, true);
+      ctx.fill();
+      ctx.strokeStyle = `rgb(${x},${y},${radius})`;
+      ctx.stroke();
+    };
+    this.update = function () {
+      // check position to prevent get lost
+      if (x + radius > innerWidth || x - radius < 0) {
+        dx = -dx;
+      }
 
-// new line circle
-ctx.beginPath();
-ctx.arc(320, 320, 50, 0, Math.PI * 2, true);
-ctx.strokeStyle = "gray";
-ctx.stroke();
-
-for (let i = 0; i < 3; i++) {
-  ctx.beginPath();
-  ctx.arc(100 * i, 500, 50, 0, Math.PI * 2, true);
-  ctx.strokeStyle = "gray";
-  ctx.stroke();
+      if (y + radius > innerHeight || y - radius < 0) {
+        dy = -dy;
+      }
+      // increase the axis
+      x += dx;
+      y += dy;
+      this.draw();
+    };
+  }
 }
+
+const circleArray = [];
+
+for (let i = 0; i < 100; i++) {
+  let x = Math.random() * innerWidth;
+  let y = Math.random() * innerHeight;
+  let radius = 20;
+
+  let dx = Math.random() * 0.3 * 8;
+  let dy = Math.random() * 0.3 * 8;
+  circleArray.push(new Circle(x, y, dx, dy, radius));
+}
+
+const animate = () => {
+  requestAnimationFrame(animate);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  circleArray.forEach((circle) => circle.update());
+};
+animate();
